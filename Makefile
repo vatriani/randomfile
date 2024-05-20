fd2e4d7ea840e47f7ad8ab64be60e0606f7acf5d
Niels Neumann <vatriani.nn@googlemail.com>
2024-05-20 12:22:22 +0200

    Windows compatibility added
    some minor fixes and if statements for Windows support

--- Makefile
+++ Makefile
@@ -1,19 +1,60 @@
+ifeq ($(OS),Windows_NT)
+	WIN += 1
+    EXECUTEABLE += randomfile.exe
+	CC="C:\mingw64\bin\gcc.exe"
+    ifeq ($(PROCESSOR_ARCHITEW6432),AMD64)
+        CCFLAGS += -D AMD64
+    else
+        ifeq ($(PROCESSOR_ARCHITECTURE),AMD64)
+            CCFLAGS += -D AMD64
+        endif
+        ifeq ($(PROCESSOR_ARCHITECTURE),x86)
+            CCFLAGS += -D IA32
+        endif
+    endif
+else
+	EXECUTEABLE += randomfile
+	CC="gcc"
+    UNAME_S := $(shell uname -s)
+    ifeq ($(UNAME_S),Linux)
+        CCFLAGS += -D LINUX
+    endif
+    ifeq ($(UNAME_S),Darwin)
+        CCFLAGS += -D OSX
+    endif
+    UNAME_P := $(shell uname -p)
+    ifeq ($(UNAME_P),x86_64)
+        CCFLAGS += -D AMD64
+    endif
+    ifneq ($(filter %86,$(UNAME_P)),)
+        CCFLAGS += -D IA32
+    endif
+    ifneq ($(filter arm%,$(UNAME_P)),)
+        CCFLAGS += -D ARM
+    endif
+endif
+
 FLAGS=-Wall -O2 -mtune=native -march=native
-#FLAGS:=-Wall
 SRCS=string.o file.o output.o lists.o
-TARGET=randomfile
 
-all: $(TARGET)
+all: $(EXECUTEABLE)
 
-randomfile: $(SRCS) main.c
-	gcc $(FLAGS) $(SRCS) -o $(TARGET) main.c
+$(EXECUTEABLE): $(SRCS) main.c
+	$(CC) $(FLAGS) $(SRCS) -o $(EXECUTEABLE) main.c
 
 spagetti: spagetti.c
-	gcc $(FLAGS) -o spagetti spagetti.c
+	$(CC) $(FLAGS) $(CCFLAGS) -o $(EXECUTEABLE) spagetti.c
+
+run:
+	$(EXECUTEABLE) ./
 
 clean:
-	rm -f *.d *.o $(TARGET)
-	rm -f *.d *.o spagetti
+ifeq ($(OS),Windows_NT)
+	del $(EXECUTEABLE)
+	del *.o
+else
+	rm -f *.d *.o $(EXECUTEABLE)
+endif
 
 install: $(TARGET)
 	cp $(TARGET) /usr/bin/$(TARGET)
--- spagetti.c
+++ spagetti.c
@@ -1,47 +1,80 @@
+/**
+ * spagetti.c - returns a random file from given or actual direcory
+ * Copyright © 2014 - Niels Neumann  <vatriani.nn@googlemail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
 
+#ifdef _WIN32
+	#include <stdlib.h> 
+	#include <time.h>
+#endif	
+
+
+// getting random number from /dev/random or on an windows enviroment from srand
 unsigned int generateRandomNumber () {
- 	unsigned int randval;
-	FILE* file;
+  unsigned int randval;
 
-	file = fopen ("/dev/random", "r");
-	if(!fread (&randval, sizeof (randval), 1, file))
-		puts("error in generateRandomNumber");
+#ifdef _WIN32  // Windows definition
+  srand ( time( NULL ) ); // seed the random number generator
+  randval = rand ( );
+#else
+  FILE* file;
 
-	fclose (file);
+  file = fopen ( "/dev/random", "r" );
+
+  if ( !fread ( &randval, sizeof ( randval ), 1, file ) )
+    puts ( "error in generateRandomNumber" );
+
+  fclose ( file );
+#endif
 
 	return randval;
 }
 
-int main () {
-	DIR *dp;
-	struct dirent *ep;
-	register unsigned int count;
-	register unsigned int random;
+int main ( int argc, char** argv ) {
+  DIR *dp;
+  struct dirent *ep;
+  register unsigned int count;
+  register unsigned int random;
 
-	count = 0;
-	random = 0;
-	dp = opendir (".");
+  count = 0;
+  random = 0;
 
-	while ( (ep = readdir (dp)) != NULL )
-		++count;
+  if ( argc > 1 ) dp = opendir ( argv[1] );
+  else dp = opendir ( "." );
 
-	rewinddir (dp);
+  while ( (ep = readdir ( dp )) != NULL ) ++count;
 
-	do {
-		random = generateRandomNumber () % (count-1);
+  rewinddir ( dp );
 
-		while ( (ep = readdir (dp) ) && (random != 0) )
-			--random;
+  do {
+    random = generateRandomNumber () % ( count -1 );
 
-	} while ( !strcmp (ep->d_name, ".") || !strcmp (ep->d_name, "..") );
+    while ( ( ep = readdir ( dp ) ) && ( random != 0 ) ) --random;
 
-	puts (ep->d_name);
+  } while ( !strcmp ( ep->d_name, "." ) || !strcmp ( ep->d_name, ".." ) );
 
-	closedir (dp);
+  puts ( ep->d_name );
 
-	return 0;
+  closedir ( dp );
+
+  return 0;
 }
